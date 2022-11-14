e39d5625b875bd3ad116d966f0793579e86752e8
Mike Cummins <Cummins.Michael.R@gmail.com>
2022-11-14 13:07:03 -0800

    Add generate_key and generate_offset

--- lib/key_generator.rb
+++ lib/key_generator.rb
@@ -1,7 +1,9 @@
+require 'date'
+
 class KeyGenerator
-  attr_reader :key, 
+  attr_reader :key,
               :offset
-  
+
   def initialize
     @key = []
     @offset = []
@@ -12,5 +14,11 @@ class KeyGenerator
       num = rand(1...9)
       @key << num
     end
+  end
+
+  def generate_offset(date = Date.today)
+    date = date.strftime('%d%m%y').to_i
+    sq_date = date**2
+    @offset = sq_date.digits.pop(4)
   end
 end
--- spec/key_generator_spec.rb
+++ spec/key_generator_spec.rb
@@ -26,4 +26,11 @@ RSpec.describe KeyGenerator do
     expect(key_generator.key).to be_a(Array)
     expect(key_generator.key.count).to eq(5)
   end
+
+  it 'generates 4 digit offset based on date DDMMYY, today by default' do
+    key_generator = KeyGenerator.new
+
+    expect(key_generator.generate_offset).to be_a(Array)
+    expect(key_generator.generate_offset.count).to eq(4)
+  end
 end
