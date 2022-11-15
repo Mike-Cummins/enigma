a41858c4794a72b24e7c86e93657345d486423f6
Mike Cummins <Cummins.Michael.R@gmail.com>
2022-11-15 13:13:13 -0800

    Add functionality for decrypt CLI

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -5,4 +5,4 @@ read_file = ARGV[0].dup.prepend('./lib/')
 write_file = ARGV[1].dup.prepend('./lib/')
 encrypted = enigma.encrypt(File.read(read_file))
 File.write(write_file, encrypted[:encryption])
-puts "Created #{ARGV[1]} with the key #{encrypted[:key]} and date #{encrypted[:date]}"
+puts "Created '#{ARGV[1]}' with the key #{encrypted[:key]} and date #{encrypted[:date]}"
